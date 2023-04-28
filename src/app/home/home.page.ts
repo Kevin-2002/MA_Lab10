77b2458fb0bcaebbfcff31f78a300fa3181382d1
unknown <kevinmcshane02@gmail.com>
2023-04-28 11:06:01 +0000

    lab10

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { Browser } from '@capacitor/browser';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+long:number = 0;
+lat:number = 0;
+time:number = 0;
+
 stories:any[]=[];
   constructor() {}
   
+  async getGPS(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log("Current position: ");
+
+    this.time = coordinates.timestamp;
+    this.long = coordinates.coords.longitude;
+    this.lat = coordinates.coords.latitude;
+
+  }
+
+  async openBrowser() {
+    await Browser.open({ url: 'http://capacitorjs.com/' });
+    };
 }
